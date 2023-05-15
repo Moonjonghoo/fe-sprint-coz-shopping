cc7155839d17b2f01f5b0330419f6529553e6a00
name <moonjonghoo@gmail.com>
2023-05-16 08:23:02 +0900

    메인페이지 +북마크기능추가 + 북마크 4개이상시 랜덤으로 화면에띄우기

--- shopping/src/component/Item.js
+++ shopping/src/component/Item.js
@@ -1,26 +1,63 @@
-import './Item.css'
+import './Item.css';
+import { useState } from 'react';
 
 function Item(props) {
-    return (
-        <div className="item">
-            <img className='image' src={props.image_url || props.brand_image_url} alt='대체이미지'></img>
-            <div className='first-line'>
-            <div className="item-title">
-             {props.type === 'Brand' ? props.brand_name : (props.type === 'Category' ? '#' + props.title : props.title)}
-        </div>
-                <div>
-                    {props.type === 'Product' ? props.discountPercentage + '%' : null}
-                    {props.type === 'Brand' ? '관심고객수' : null}
-                </div>
-            </div>
-            <div className='price'>{props.price || props.follower}</div>
-            <div style={{ clear: "both" }}></div>
-            <div>{props.sub_title}</div>
-            <div></div>
+  const [isBookmarked, setIsBookmarked] = useState(false);
 
+  const handleBookmark = () => {
+    setIsBookmarked(!isBookmarked);
+
+   
+    const bookmarkedItems = JSON.parse(localStorage.getItem('bookmarkedItems')) || [];
+    if (isBookmarked) {
+      const updatedItems = bookmarkedItems.filter(item => item.id !== props.id);
+      localStorage.setItem('bookmarkedItems', JSON.stringify(updatedItems));
+      console.log('제거되었습니다.')
+    } else {
+      const newItem = {
+        id:props.id,
+        title: props.title,
+        brand_image_url:props.brand_image_url,
+        brand_name:props.brand_name,
+        discountPercentage:props.discountPercentage,
+        follower:props.follower,
+        image_url:props.image_url,
+        price:props.price,
+        sub_title:props.sub_title,
+        type:props.type
+      };
+      bookmarkedItems.push(newItem);
+      localStorage.setItem('bookmarkedItems', JSON.stringify(bookmarkedItems));
+      console.log('추가되었습니다.')
+    }
+  };
+
+  return (
+    <div className="item">
+      <div className="whole-wrapper">
+        <div className="image-wrapper">
+          <img className="image" src={props.image_url || props.brand_image_url} alt="대체이미지" />
         </div>
-    );
+        <div className="bookmark-wrapper">
+          <button className="bookmark-button" onClick={handleBookmark}>
+            <i className={`fas fa-star${isBookmarked ? ' bookmarked' : ''}`}></i>
+          </button>
+        </div>
+      </div>
+      <div className="first-line">
+        <div className="item-title">
+          {props.type === 'Brand' ? props.brand_name : props.type === 'Category' ? '#' + props.title : props.title}
+        </div>
+        <div>
+          {props.type === 'Product' ? props.discountPercentage + '%' : null}
+          {props.type === 'Brand' ? '관심고객수' : null}
+        </div>
+      </div>
+      <div className="price">{props.price || props.follower}</div>
+      <div style={{ clear: 'both' }}></div>
+      <div>{props.sub_title}</div>
+    </div>
+  );
 }
 
 export default Item;
-
--- shopping/src/component/List.js
+++ shopping/src/component/List.js
@@ -1,46 +1,64 @@
-import './List.css'
+import './List.css';
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Item from './Item';
 
-
 function List() {
-    const [data, setData] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [bookmark, setBookmark] = useState([]);
 
-    useEffect(() => {
-        async function fetchData() {
-            try {
-                const response = await axios.get('http://cozshopping.codestates-seb.link/api/v1/products?count=4');
-                setData(response.data);
-            } catch (error) {
-                console.error(error);
-            } finally {
-                setIsLoading(false);
-            }
-        }
-        fetchData();
-    }, []);
-
-    if (isLoading) {
-        return <div>Loading...</div>;
-
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await axios.get('http://cozshopping.codestates-seb.link/api/v1/products?count=4');
+        setData(response.data);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setIsLoading(false);
+      }
     }
-    return (
-        <div className='mainlist'>
-            {console.log(data)}
-            <h4>상품리스트</h4>
-            <ul className='itemlist'>
-                {data.map(item => (
-                    <Item {...item}></Item>
-                ))}
+    fetchData();
+  }, []);
 
-            </ul>
-            
-        </div>
-    );
+  useEffect(() => {
+   
+    const bookmarkedItems = JSON.parse(localStorage.getItem('bookmarkedItems')) || [];
+    const randomBookmarks = getRandomElements(bookmarkedItems, 4); //랜덤저장
+    setBookmark(randomBookmarks);
+  }, []);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div className="mainlist">
+      <h4>상품리스트</h4>
+      <ul className="itemlist">
+        {data.map(item => (
+          <Item key={item.id} {...item} />
+        ))}
+      </ul>
+      <h4>북마크리스트</h4>
+      <ul className="itemlist">
+        {bookmark.map(item => (
+          <Item key={item.id} {...item} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+//랜덤정렬
+function getRandomElements(arr, n) {
+  if (n >= arr.length) {
+    return arr; // 
+  } else {
+    const shuffled = arr.sort(() => 0.5 - Math.random()); // 섞기
+    return shuffled.slice(0, n); // n개의 요소 선택
+  }
 }
 
 export default List;
-
-
