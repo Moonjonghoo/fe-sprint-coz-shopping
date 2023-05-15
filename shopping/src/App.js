25ed1b43cf2e628a68e582ae73036dec31cba02f
name <moonjonghoo@gmail.com>
2023-05-15 12:56:19 +0900

    하단

--- shopping/src/App.js
+++ shopping/src/App.js
@@ -5,11 +5,13 @@ import Header from './component/Header';
 import Main from './page/Main';
 import Bookmark from './page/Bookmark';
 import Product from './page/Product';
+import Footer from './component/footer';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      
      
       <Routes>
         <Route path="/" element={<Main></Main>}></Route>
@@ -17,7 +19,7 @@ function App() {
         <Route path='/bookmark' element={<Bookmark></Bookmark>}></Route>
         
       </Routes>
-      
+      <Footer></Footer>
     </div>
   );
 }
---
+++ shopping/src/component/footer.js
@@ -0,0 +1,16 @@
+import './footer.css';
+
+function Footer() {
+  return (
+    <div>
+      <div className="first">
+        <p>개인정보 처리방침</p>
+        <div className="line-footer"></div>
+        <p>이용약관</p>
+      </div>
+      <p className='second'>All right reserved @codestates</p>
+    </div>
+  );
+}
+
+export default Footer;
